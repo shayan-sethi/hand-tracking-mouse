4fda3817ed83b113aa03a745dafa6de66c384a56
shayan-sethi <shayansethi29@gmail.com>
2025-05-28 18:30:12 +0530

    Update hand_tracking_mouse.py

--- hand_tracking_mouse.py
+++ hand_tracking_mouse.py
@@ -2,14 +2,15 @@ import cv2
 import mediapipe as mp
 import pyautogui
 import time
+import math
 
 capturedhands = mp.solutions.hands.Hands(max_num_hands=1, min_detection_confidence=0.7)
 drawing = mp.solutions.drawing_utils
 cam = cv2.VideoCapture(0)
 screenwidth, screenheight = pyautogui.size()
 
-prev_mouse_x, prev_mouse_y = 0, 0
 x1 = y1 = x2 = y2 = 0
+
 while True:
     success, image = cam.read()
     if not success:
@@ -24,7 +25,7 @@ while True:
 
     if hands:
         for hand in hands:
-            drawing.draw_landmarks(image, hand)
+            drawing.draw_landmarks(image, hand, mp.solutions.hands.HAND_CONNECTIONS)
             onehand = hand.landmark
             for id, lm in enumerate(onehand):
                 x = int(lm.x * imagewidth)
@@ -33,29 +34,29 @@ while True:
                 if id == 8:
                     mouse_x = int(screenwidth / imagewidth * x)
                     mouse_y = int(screenheight / imageheight * y)
-                    if abs(mouse_x - prev_mouse_x) > 5 or abs(mouse_y - prev_mouse_y) > 5:
-                        pyautogui.moveTo(mouse_x, mouse_y)
-                        prev_mouse_x, prev_mouse_y = mouse_x, mouse_y
+                    pyautogui.moveTo(mouse_x, mouse_y)
 
                     x1 = x
                     y1 = y
-
                     cv2.circle(image, (x, y), 10, (0, 255, 255), -1)
 
                 if id == 4:
                     cv2.circle(image, (x, y), 10, (0, 255, 255), -1)
                     x2 = x
                     y2 = y
-        dist = y2-y1
-        if(dist<20):
-         pyautogui.click()
+
+        dist = math.hypot(x2 - x1, y2 - y1)
+        if dist < 40:
+            pyautogui.click()
+            time.sleep(0.2)
 
     cv2.imshow("hand", image)
     key = cv2.waitKey(1)
-    if key == 27: #Press Esc Key to break loop
+    if key == 27:
         break
 
     time.sleep(0.01)
 
 cam.release()
 cv2.destroyAllWindows()
+
